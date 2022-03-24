fa8b4ebd1f7cb186d996e44617a28166485a0331
hlee443 <55458700+hlee443@users.noreply.github.com>
2022-03-23 18:11:32 -0700

    Update databaseConnection.js

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,6 +1,6 @@
 const MongoClient = require("mongodb").MongoClient;
 const is_heroku = process.env.IS_HEROKU || false;
-const herokuURI = "mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.2asjl.mongodb.net/lab_example?retryWrites=true&w=majority"
+const herokuURI = "mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.2asjl.mongodb.net/users?retryWrites=true&w=majority"
 const localURI = "mongodb://localhost/?authSource=admin&retryWrites=true&w=majority;"
 if (is_heroku) {
        var database = new MongoClient(herokuURI,
