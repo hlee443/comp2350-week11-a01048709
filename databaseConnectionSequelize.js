de14fcb76d02a0760542918a69ebca05bcec22d3
hlee443 <55458700+hlee443@users.noreply.github.com>
2022-03-28 15:01:47 -0700

    Update databaseConnectionSequelize.js

--- databaseConnectionSequelize.js
+++ databaseConnectionSequelize.js
@@ -1,8 +1,8 @@
 const is_heroku = process.env.IS_HEROKU || false;
 
-const dbConfigHeroku = "mysql://b1ab7fb2ee03bc:2a484a2d@us-cdbr-east-03.cleardb.com/heroku_3d208ad4bd6f421";
+const dbConfigHeroku = "mongodb+srv://theMongoAdmin:<password>@cluster0.2asjl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-const dbConfigLocal = "mysql://root:Password@localhost/lab_example";
+const dbConfigLocal = "mongodb://localhost/?authSource=admin&retryWrites=true&w=majority;";
 
 if (is_heroku) {
 	var databaseConnectionString = dbConfigHeroku;
@@ -12,4 +12,4 @@ else {
 }
 
 module.exports = databaseConnectionString;
-		
+		https://github.com/hlee443/comp2350-week11-a01048709/blob/main/databaseConnectionSequelize.js
